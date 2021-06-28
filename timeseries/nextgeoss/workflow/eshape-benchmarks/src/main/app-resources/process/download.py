5bc36ed9d74bb0574d6ea912d071604e7fb56049
janssenb <bram.janssen@vito.be>
2021-06-28 08:38:03 +0200

    Added output param to download script

--- timeseries/nextgeoss/workflow/eshape-benchmarks/src/main/app-resources/process/download.py
+++ timeseries/nextgeoss/workflow/eshape-benchmarks/src/main/app-resources/process/download.py
@@ -17,6 +17,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 # ---------------------------------------------------------
 parser = argparse.ArgumentParser(description='Download a file using the TerraScope client')
 parser.add_argument('-p', required=True, action='store', nargs='+', dest='products', help='Product to download')
+parser.add_argument('-o', required=True, action='store', dest='output', help='Output path to store files')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -24,5 +25,5 @@ if __name__ == '__main__':
     catalogue.authenticate_non_interactive(username='eshape-benchmark', password='rD2bswtP%v#Bb6wU%3SF')
     basename = args.products[0].split('/')[9]
     for product in args.products:
-        catalogue.download_file(ProductFile(href=product, length=None), '/tmp')
+        catalogue.download_file(ProductFile(href=product, length=None), args.output)
     print(basename)
