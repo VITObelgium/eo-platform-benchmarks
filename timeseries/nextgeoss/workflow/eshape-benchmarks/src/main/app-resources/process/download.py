c5dc4d81550d79990ca96572888b663f565aac9e
janssenb <bram.janssen@vito.be>
2021-06-28 08:33:07 +0200

    Added download script

---
+++ timeseries/nextgeoss/workflow/eshape-benchmarks/src/main/app-resources/process/download.py
@@ -0,0 +1,28 @@
+import argparse
+import logging
+
+from terracatalogueclient import Catalogue, ProductFile
+
+"""
+
+"""
+
+# ---------------------------------------------------------
+#                  Logging
+# ---------------------------------------------------------
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# ---------------------------------------------------------
+#                  Argument parser
+# ---------------------------------------------------------
+parser = argparse.ArgumentParser(description='Download a file using the TerraScope client')
+parser.add_argument('-p', required=True, action='store', nargs='+', dest='products', help='Product to download')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    catalogue = Catalogue()
+    catalogue.authenticate_non_interactive(username='eshape-benchmark', password='rD2bswtP%v#Bb6wU%3SF')
+    basename = args.products[0].split('/')[9]
+    for product in args.products:
+        catalogue.download_file(ProductFile(href=product, length=None), '/tmp')
+    print(basename)
